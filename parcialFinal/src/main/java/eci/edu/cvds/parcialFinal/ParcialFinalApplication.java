83c6f6451264ea1da2626d988bace9efbb561201
LauraGutierrezr <114092766+LauraGutierrezr@users.noreply.github.com>
2025-05-23 15:17:41 -0500

    Primer Commit Spring

---
+++ parcialFinal/src/main/java/eci/edu/cvds/parcialFinal/ParcialFinalApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.cvds.parcialFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialFinalApplication.class, args);
+	}
+
+}
---
+++ parcialFinal/src/test/java/eci/edu/cvds/parcialFinal/ParcialFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.cvds.parcialFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
